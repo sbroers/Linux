53557fc19b8114d115577bc943cfbd3261e6de02
sbroers <44463486+sbroers@users.noreply.github.com>
2019-05-27 13:53:42 +0200

    Update client.sh

--- Script/client.sh
+++ Script/client.sh
@@ -3,6 +3,8 @@ echo 'GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"' >> /etc/default/grub
 update-grub
 wget https://raw.githubusercontent.com/sbroers/Linux/master/Script/wallpaper.png
 wget https://raw.githubusercontent.com/sbroers/Linux/master/Script/logo.png
+wget https://raw.githubusercontent.com/sbroers/Linux/master/Script/wbswall.jpg
 mv wallpaper.png /usr/share/plymouth/themes/xubuntu-logo/
 mv logo.png /usr/share/plymouth/themes/xubuntu-logo/
+mv wbswall.png /usr/share/xfce4/backdrops/xubuntu-bionic.png
 reboot now
