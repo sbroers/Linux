cd0f58eebdfcbfa602633920ca5fb2d4dce40eb4
sbroers <44463486+sbroers@users.noreply.github.com>
2019-05-28 11:28:30 +0200

    Update sqmail.sh

--- Script/sqmail.sh
+++ Script/sqmail.sh
@@ -1,2 +1,2 @@
 mkdir -p /var/www/html/sqmail
-wget -qO- https://netix.dl.sourceforge.net/project/squirrelmail/stable/1.4.22/squirrelmail-webmail-1.4.22.tar.gz | tar xz -C /var/www/html/sqmail --strip-components 1 && chown -r www-data /var/www/html/sqmail
+wget -qO- https://netix.dl.sourceforge.net/project/squirrelmail/stable/1.4.22/squirrelmail-webmail-1.4.22.tar.gz | tar xz -C /var/www/html/sqmail --strip-components 1 && chown -R www-data /var/www/html/sqmail
