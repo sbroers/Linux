89ffe9e43619cf8cc555fbc0db0930206797f207
sbroers <44463486+sbroers@users.noreply.github.com>
2019-05-28 11:25:24 +0200

    Create sqmail.sh

---
+++ Script/sqmail.sh
@@ -0,0 +1,1 @@
+wget -qO- https://netix.dl.sourceforge.net/project/squirrelmail/stable/1.4.22/squirrelmail-webmail-1.4.22.tar.gz | tar xz -C /var/www/html/sqmail --strip-components 1 && chown -r www-data /var/www/html/sqmail
