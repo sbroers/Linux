1755d60827aa6e9e01afa5bf57458cb4c668bb4f
sbroers <44463486+sbroers@users.noreply.github.com>
2019-06-06 07:38:10 +0200

    Update ubuntu-hyperv.sh

--- Script/ubuntu-hyperv.sh
+++ Script/ubuntu-hyperv.sh
@@ -1,8 +1,8 @@
-sudo echo "hv_vmbus" >> nano /etc/initramfs-tools/modules
-sudo echo "hv_storvsc" >> nano /etc/initramfs-tools/modules
-sudo echo "hv_blkvsc" >> nano /etc/initramfs-tools/modules
-sudo echo "hv_netvsc" >> nano /etc/initramfs-tools/modules
+sudo echo "hv_vmbus" >> /etc/initramfs-tools/modules
+sudo echo "hv_storvsc" >> /etc/initramfs-tools/modules
+sudo echo "hv_blkvsc" >> /etc/initramfs-tools/modules
+sudo echo "hv_netvsc" >> /etc/initramfs-tools/modules
 #
-sudo apt update && install linux-virtual linux-cloud-tools-virtual linux-tools-virtual
+sudo apt update && sudo apt install linux-virtual linux-cloud-tools-virtual linux-tools-virtual
 update-initramfs -u
 sudo reboot now
